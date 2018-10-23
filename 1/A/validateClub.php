9abdbe1a18f0b6cfdd9d364a008bd37f2a077176
DManstrator <DManstrator@gmail.com>
2018-10-23 21:29:20 +0200

    Began with Transactions and Prepared Statements

--- 1/A/validateClub.php
+++ 1/A/validateClub.php
@@ -9,31 +9,40 @@ if (!isset($_POST['club']))  {
 
 $con = open_connection($servername, $username, $password, $database);
 
-// TODO Transactions and Prepared Statements
 $club_id = $_POST['club'];  // TODO if $club_id == 'other' || id not in Table (HTML Injection)
-$sql = "UPDATE football_clubs SET club_votes = club_votes + 1 WHERE club_id = $club_id";
 
-if (!mysqli_query($con, $sql)) {
+// Lock Database
+mysqli_autocommit($con, false);
+//mysqli_begin_transaction($con);
+
+$sql = "UPDATE football_clubs SET club_votes = club_votes + 1 WHERE club_id = ?;";
+$stmt = mysqli_prepare($con, $sql);
+if (!$stmt) {
 	die("An Error occurred while updating the Football Table. Error: '" . mysqli_error($con)) . "'";
 }
 
-$sql = "SELECT *, (SELECT SUM(`club_votes`) FROM football_clubs) as fans FROM football_clubs WHERE `club_id` = $club_id;";
+mysqli_stmt_bind_param($stmt, 'i', $club_id);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_close($stmt);
 
-$sql_result = mysqli_query($con, $sql);
-if (!$sql_result) {
+$sql = "SELECT *, (SELECT SUM(`club_votes`) FROM football_clubs) as fans FROM football_clubs WHERE `club_id` = ?;";
+$stmt = mysqli_prepare($con, $sql);
+if (!$stmt) {
 	die("An Error occurred while evaluating the response. Error: '" . mysqli_error($con)) . "'";
 }
+mysqli_stmt_bind_param($stmt, "i", $club_id);
+mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_result($stmt, $club_id, $club_name, $club_votes, $fans);
+mysqli_stmt_fetch($stmt);
+mysqli_stmt_close($stmt);
+// Commit
+mysqli_commit($con);
 
-$row = mysqli_fetch_assoc($sql_result);
-$club_name = $row['club_name'];
-$vote = $row['club_votes'];
-$fans = $row['fans'];
-
-mysqli_free_result($sql_result);
+// Unlock Database
 mysqli_close($con);
 
 echo "<center>Du hast erfolgreich für <b>$club_name</b> gestimmt!<br>
-<b>$club_name</b> hat bereits <b>$vote Fans</b>. :D <br>
+<b>$club_name</b> hat bereits <b>$club_votes Fans</b>. :D <br>
 Insgesamt haben bereits $fans Personen abgestimmt.</center>";
 // TODO Proper HTML
 ?>
