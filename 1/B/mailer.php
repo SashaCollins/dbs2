e8669a6c63ea9ec2c307fc112c9a730aa9998f4d
DManstrator <DManstrator@gmail.com>
2018-11-07 20:45:08 +0100

    Suppressed echo of SQL stmt (debug only)

--- 1/B/mailer.php
+++ 1/B/mailer.php
@@ -76,7 +76,7 @@ if (isset($_POST['submit'])) {
 			}
 		}
 		$sql .= " GROUP BY p.person_id ORDER BY p.person_id;";
-		echo $sql;
+		//echo $sql;
 		
 		$result = mysqli_query($con, $sql);
 		if (!$result) {
