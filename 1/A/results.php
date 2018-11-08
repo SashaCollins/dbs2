7a6ac08d0045471612ac0302ca2b2d781caa486f
DManstrator <DManstrator@gmail.com>
2018-11-08 11:10:36 +0100

    Improved Design of Football Club Vote Results

--- 1/A/results.php
+++ 1/A/results.php
@@ -30,14 +30,14 @@ while ($row = mysqli_fetch_assoc($result))  {
 	$votes = $row['club_votes'];
 	$percent = $row['percent'];
 	$fans = $row['fans'];
-	$output .= "\t\t$name | $votes | $percent<br />\n";
+	$output .= "\t\t\t<tr><td class='name'>$name</td><td>$votes</td><td>$percent</td></tr>\n";
 	
 	if ($votes != 0 || !$removeNull)  {
 		array_push($data, $votes);
 		array_push($legend, $name);
 	}
 }
-$output .= "\t\tTotal | $fans | 100%<br />\n";
+$output .= "\t\t\t<tr class='footer'><th style='text-align: right;'>Total:</th><td>$fans</td><td>100%</td></tr>\n";
 
 $filename = "fan-statistic.png";
 if ($fans != 0)  {
@@ -52,13 +52,50 @@ mysqli_close($con);
 	<head>
 		<title>Auswertung der Statistik | Fussballfans</title>
 		<meta charset="UTF-8" />
+		<style>
+			td  {
+				text-align: right;
+			}
+			td.name  {
+				text-align: center;
+			}
+			tr.header  {
+				border-bottom: 2px solid;
+			}
+			tr.footer  {
+				border-top: 2px solid;
+			}
+			td, th  {
+				padding-left: 5px;
+				padding-right: 5px;
+			}
+			table  {
+				border-collapse: collapse;
+			}
+			tr  {
+				border: 1px solid;
+			}
+			table td, table th  {
+				border-left: 2px solid;
+			}
+			table td:first-child, th:first-child  {
+				border-left: none;
+			}
+		</style>
 	</head>
 	<body>
+		<center>
+		<table style="border-style: solid; border-width: 3px;">
+			<tr class="header"><th>Vereinsname</th><th>Anzahl Stimmen</th><th>Prozentualer Anteil</th></tr>
 <?php
-		echo $output;
+			echo $output;
+?>
+		</table>
+<?php
 		if ($fans != 0)  {
-			echo '<br /><img src="'.$filename.'" />';
+			echo "\t\t".'<br /><img src="'.$filename.'" />';
 		}
 ?>
+		</center>
 	</body>
 </html>
